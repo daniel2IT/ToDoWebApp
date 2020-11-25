83d8c9b231c9184ae085e6d116a416a7244b2316
Daniel <50612327+daniel2IT@users.noreply.github.com>
2020-11-26 00:46:37 +0200

    Added Models

---
+++ ToDoWebApp/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoWebApp.Models
+{
+    public class TodoItem
+    {
+
+        int Id;
+        string Name;
+        string Description;
+
+        int[] priority = new int[5] { 1, 2, 3, 4, 5 };
+
+    }
+}
